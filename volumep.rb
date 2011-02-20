552a377f8d17b6b842dc052246f16d639b7d5768
ewryan <ewryan@gmail.com>
2011-02-20 10:22:04 -0700

    er: initial checkin

---
+++ volumep.rb
@@ -0,0 +1,36 @@
+require 'bundler'
+
+Bundler.require
+
+get '/' do
+  @vol = get_volume
+  haml :index
+end
+
+get '/mute' do
+  set_volume 0
+  redirect '/'
+end
+
+get '/max' do
+  set_volume 100
+  redirect '/'
+end
+
+post '/set_volume' do
+  set_volume params[:vol]
+  redirect '/'
+end
+
+def set_volume vol
+  if vol
+    `osascript -e "set volume output volume #{vol}"`
+  else
+    puts "Error, no volume specified"
+  end
+end
+
+def get_volume
+  vol = `osascript -e "output volume of (get volume settings)"`
+  vol.to_i if vol
+end
